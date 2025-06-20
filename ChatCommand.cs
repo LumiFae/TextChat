817decf65d85a7b050f5e2fa1b8d5f5f89175958
Lumi <me@jxtq.moe>
2025-06-20 22:34:59 +0100

    feat: chat command

---
+++ ChatCommand.cs
@@ -0,0 +1,27 @@
+﻿using CommandSystem;
+using LabApi.Features.Wrappers;
+
+namespace TextChat
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class ChatCommand : ICommand
+    {
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+            if (player == null)
+            {
+                response = "Only players can send messages.";
+                return false;
+            }
+            
+            string resp = Events.TrySendMessage(player, string.Join(" ", arguments));
+            response = resp ?? "Success!";
+            return resp == null;
+        }
+
+        public string Command { get; } = "chat";
+        public string[] Aliases { get; } = Array.Empty<string>();
+        public string Description { get; } = "Send a text chat message";
+    }
+}
