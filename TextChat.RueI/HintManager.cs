09832efbb05db0fd7be25990423d8119520d2398
Lumi <me@jxtq.moe>
2025-06-21 14:37:31 +0100

    feat: ruei text chat

---
+++ TextChat.RueI/HintManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Drawing;
+using System.Text;
+using LabApi.Features.Stores;
+using LabApi.Features.Wrappers;
+using MEC;
+using NorthwoodLib.Pools;
+using PlayerRoles;
+using RueI.Displays;
+using RueI.Elements;
+using RueI.Extensions.HintBuilding;
+using RueI.Parsing.Enums;
+
+namespace TextChat.RueI
+{
+    internal sealed class DisplayDataStore : CustomDataStore
+    {
+        public DisplayDataStore(Player player) : base(player)
+        {
+            Player = player;
+            Display = new(player.ReferenceHub);
+            Validate();
+        }
+
+        public Player Player;
+
+        public Display Display;
+
+        public void Validate()
+        {
+            Display.Elements.Clear();
+            Display.Elements.Add(Player.Role == RoleTypeId.Spectator ? HintManager.SpectatorElement : HintManager.ScpElement);
+        }
+
+        public static void UpdateAndValidateAll()
+        {
+            foreach (Player player in Player.ReadyList.Where(player => player.Role == RoleTypeId.Spectator || player.IsSCP))
+            {
+                DisplayDataStore store = player.GetDataStore<DisplayDataStore>();
+                store.Validate();
+                store.Display.Update();
+            }
+        }
+    }
+    
+    public static class HintManager
+    {
+        private static List<string> ActiveSpectatorMessages = new();
+        private static List<string> ActiveScpMessages = new();
+
+        internal static readonly DynamicElement ScpElement = new(ScpContent, 200);
+        internal static readonly DynamicElement SpectatorElement = new(SpectatorContent, 200);
+        
+        internal static void AddSpectatorChatMessage(string text)
+        {
+            ActiveSpectatorMessages.Add(text);
+            DisplayDataStore.UpdateAndValidateAll();
+            
+            Timing.CallDelayed(Plugin.Instance.Config!.MessageExpireTime, () =>
+            {
+                ActiveSpectatorMessages.Remove(text);
+                DisplayDataStore.UpdateAndValidateAll();
+            });
+        }
+
+        internal static void AddScpChatMessage(string text)
+        {
+            ActiveScpMessages.Add(text);
+            DisplayDataStore.UpdateAndValidateAll();
+            
+            Timing.CallDelayed(Plugin.Instance.Config!.MessageExpireTime, () =>
+            {
+                ActiveScpMessages.Remove(text);
+                DisplayDataStore.UpdateAndValidateAll();
+            });
+        }
+
+        private static string Content<T>(List<T> list)
+        {
+            StringBuilder builder = StringBuilderPool.Shared.Rent();
+
+            int fontSize = 28;
+            
+            builder.SetAlignment(HintBuilding.AlignStyle.Left);
+            builder.SetSize(fontSize);
+            // 40.665 is RueI's default line height
+            builder.AddVOffset((list.Count - 1) * 40.665f);
+            
+            builder.Append(string.Join("\n", list));
+            
+            builder.CloseVOffset();
+            builder.CloseSize();
+            builder.CloseAlign();
+
+            return StringBuilderPool.Shared.ToStringReturn(builder);
+        }
+        
+        private static string ScpContent(DisplayCore core) => Content(ActiveScpMessages);
+        
+        private static string SpectatorContent(DisplayCore core) => Content(ActiveSpectatorMessages);
+    }
+}
---
+++ TextChat.RueI/Plugin.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel;
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.Handlers;
+using LabApi.Features.Wrappers;
+using LabApi.Loader.Features.Plugins;
+using PlayerRoles;
+
+namespace TextChat.RueI
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Instance { get; private set; }
+        
+        public override void Enable()
+        {
+            Instance = this;
+            Events.SentGlobalMessage += OnSentGlobalMessage;
+            PlayerEvents.ChangingRole += OnChangingRole;
+        }
+
+        public override void Disable()
+        {
+            Instance = null;
+            Events.SentGlobalMessage -= OnSentGlobalMessage;
+            PlayerEvents.ChangingRole -= OnChangingRole;
+        }
+
+        private void OnChangingRole(PlayerChangingRoleEventArgs ev) => DisplayDataStore.UpdateAndValidateAll();
+
+        private void OnSentGlobalMessage(Player player, string text)
+        {
+            if(player.Role == RoleTypeId.Spectator) HintManager.AddSpectatorChatMessage(string.Format(Config!.Prefix, player.DisplayName) + text);
+            else if(player.Team == Team.SCPs) HintManager.AddScpChatMessage(string.Format(Config!.Prefix, player.DisplayName) + text);
+        }
+
+        public override string Name { get; } = "TextChat.RueI";
+        public override string Description { get; } = "Adds global chat functionality using RueI for hints.";
+        public override string Author { get; } = "LumiFae";
+        public override Version Version { get; } = new (1, 0, 0);
+        public override Version RequiredApiVersion { get; } = new(1, 0, 2);
+    }
+    
+    public class Config
+    {
+        [Description("The amount of time til a message expires/deletes.")]
+        public float MessageExpireTime { get; set; } = 10;
+        
+        [Description("The content that appears before a message, {0} is the player nickname")]
+        public string Prefix { get; set; } = "<color=green>{0}: </color>";
+    }
+}
