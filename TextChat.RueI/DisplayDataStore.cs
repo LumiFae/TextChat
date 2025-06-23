f326007c6d71182b6565efff9af21d98048f24ad
Jay <me@jxtq.moe>
2025-06-23 11:30:05 +0100

    feat: standalone display store

---
+++ TextChat.RueI/DisplayDataStore.cs
@@ -0,0 +1,44 @@
+using LabApi.Features.Stores;
+using LabApi.Features.Wrappers;
+using PlayerRoles;
+using PlayerRoles.Subroutines;
+using RueI.Displays;
+
+namespace TextChat.RueI
+{
+    internal sealed class DisplayDataStore : CustomDataStore
+    {
+        public DisplayDataStore(Player player) : base(player)
+        {
+            Player = player;
+            Display = new(player.ReferenceHub);
+            Cooldown = new();
+            Validate();
+        }
+
+        public Player Player;
+
+        public Display Display;
+
+        public AbilityCooldown Cooldown;
+
+        public void Validate()
+        {
+            Display.Elements.Clear();
+            Display.Elements.Add(Player.Role == RoleTypeId.Spectator
+                ? HintManager.SpectatorElement
+                : HintManager.ScpElement);
+        }
+
+        public static void UpdateAndValidateAll()
+        {
+            foreach (Player player in Player.ReadyList.Where(player =>
+                         player.Role == RoleTypeId.Spectator || player.IsSCP))
+            {
+                DisplayDataStore store = player.GetDataStore<DisplayDataStore>();
+                store.Validate();
+                store.Display.Update();
+            }
+        }
+    }
+}
