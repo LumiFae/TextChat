58a03c86f834dc33b8e87ab823aff63f8e3f5648
Lumi <me@jxtq.moe>
2025-06-23 20:46:41 +0100

    feat: new event args

---
+++ TextChat/API/EventArgs/SendingProximityHintEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using LabApi.Features.Wrappers;
+
+namespace TextChat.API.EventArgs
+{
+    public class SendingProximityHintEventArgs : IEventArgs
+    {
+        public SendingProximityHintEventArgs(Player player, string text)
+        {
+            Player = player;
+            Text = text;
+            IsAllowed = true;
+        }
+        
+        public Player Player { get; }
+        public string Text { get; }
+        
+        public bool IsAllowed { get; set; }
+    }
+}
