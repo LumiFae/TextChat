1aa3483839a9ee71e8c5556af17fb657a3ada0e5
Lumi <me@jxtq.moe>
2025-06-25 10:12:27 +0100

    docs: more config docs

--- TextChat/Config.cs
+++ TextChat/Config.cs
@@ -4,17 +4,19 @@ namespace TextChat
 {
     public class Config
     {
-        [Description("The height offset of the text based on the player")]
+        [Description("The height offset of the text based on the player.")]
         public float HeightOffset { get; set; } = 0.9f;
 
-        [Description("The size of the text")] public float TextSize { get; set; } = 0.1f;
+        [Description("The size of the text.")] 
+        public float TextSize { get; set; } = 0.1f;
 
-        [Description("How long will it take for a message to disappear, or switch to the next message")]
+        [Description("How long will it take for a message to disappear, or switch to the next message.")]
         public float MessageExpireTime { get; set; } = 7;
 
+        [Description("The max allowed length of a message.")]
         public int MaxMessageLength { get; set; } = 34;
 
-        [Description("A list of words/multiple words that are banned")]
+        [Description("A list of words/multiple words that are banned.")]
         public string[] BannedWords { get; set; } = Array.Empty<string>();
 
         [Description("This option tells the client whether their command has failed if true. If false then it won't say it failed, but will continue to show the right translation.")]
